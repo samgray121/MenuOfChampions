eec43943c6fa105b8c7f0bb0e393108c00fc62b2
sam.gray121@gmail.com <Mrtwister123!>
2022-01-05 18:59:31 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String pizza, pie, omelet;
+        double priceOfPizza, priceOfPie, priceOfOmelet;
+
+        pizza = "Slice of Cheese Pizza";
+        pie = "Slice of Blueberry Pie";
+        omelet = "Traditional Omelet";
+
+        priceOfPizza = 8.75;
+        priceOfPie = 9.55;
+        priceOfOmelet = 10.25;
+
+        System.out.println("Welcome to Sam's Cafe!");
+        System.out.println("Today's Menu Is...");
+        System.out.println("           ");
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("           ");
+        System.out.println(pizza + " $" + priceOfPizza);
+        System.out.println(pie + " $" + priceOfPie);
+        System.out.println(omelet + " $" + priceOfOmelet);
+
+
+
+
+
+    }
+}
